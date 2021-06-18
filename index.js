0d52d21d0792da6fd03647b1005671c912663283
Victor <victorrivera714@yahoo.com>
2021-06-18 12:39:36 -0700

    commit tree bc44fe8b275a9a20197c46d1f40330a769293c7d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+ 
+function writeCards(names){
+
+    let people = []
+
+    for (let i = 0; i< names.length; i++){
+       people.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    }
+
+    return people
+
+}
+
+
+
+
+
+function countDown (number){
+
+    while (number >= 0){
+        console.log (number)
+         number--
+    }
+    
+}
